f6696bd021b4348bb560397b8190bed757bc637e
Arya Das <aryadas98@gmail.com>
2017-12-12 15:13:24 +0530

    Added ids and checkbox control.

--- app/src/main/java/com/example/planetracker/MainActivity.java
+++ app/src/main/java/com/example/planetracker/MainActivity.java
@@ -8,8 +8,20 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView acc_out;
+    private TextView loc_out;
+    private TextView alt_out;
+    private TextView hed_out;
+
+    private boolean acc_log = false;
+    private boolean loc_log = false;
+    private boolean alt_log = false;
+    private boolean hed_log = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,4 +29,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    public void onCheckClick(View view) {
+        boolean checked = ((CheckBox) view).isChecked();
+        ((CheckBox)view).setText(checked ? "Logging" : "Not Logging");
+
+        switch(view.getId()) {
+            case R.id.acc_log: acc_log = checked; break;
+            case R.id.loc_log: loc_log = checked; break;
+            case R.id.alt_log: alt_log = checked; break;
+            case R.id.hed_log: hed_log = checked; break;
+        }
+    }
 }
